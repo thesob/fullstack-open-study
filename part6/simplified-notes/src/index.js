7ec973ccccec8cc39bd87cedb9cf78d5fbbceab0
thesob <patricio.sobradogmail.com>
2023-05-25 13:18:32 -0400

    Before moving db initialization to App

--- part6/simplified-notes/src/index.js
+++ part6/simplified-notes/src/index.js
@@ -4,8 +4,10 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
-import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+
+import noteService from './services/notes'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 
 const store = configureStore({
   reducer: {
@@ -13,6 +15,10 @@ const store = configureStore({
     filter: filterReducer
   }
 })
+
+noteService.getAll().then(notes =>
+  store.dispatch(setNotes(notes))
+)
 
 console.log(store.getState())
 
