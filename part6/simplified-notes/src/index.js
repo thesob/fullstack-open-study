abad76fcd55c9b6be85957fa6ba97dc46d76ebe0
thesob <patricio.sobradogmail.com>
2023-05-25 16:22:35 -0400

    Redux thunk async changes

--- part6/simplified-notes/src/index.js
+++ part6/simplified-notes/src/index.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
+import store from './reducers/store'
 import App from './App'
 
-import filterReducer from './reducers/filterReducer'
-import noteReducer from './reducers/noteReducer'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/simplified-notes/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux"
-import noteReducer from "./noteReducer"
-import filterReducer from "./filterReducer"
-
-export default combineReducers({
-    notes: noteReducer,
-    filter: filterReducer
-})
