5d57738df4258a45aa67d06b5038d992abc4bd21
thesob <patricio.sobradogmail.com>
2023-06-03 12:00:10 -0400

    With promise-polyfill

--- part7/webpack/src/App.js
+++ part7/webpack/src/App.js
@@ -1,9 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './index.css'
+import PromisePolyfill from 'promise-polyfill'
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 const App = () => {
     const [counter, setCounter] = useState(0)
     const [values, setValues] = useState([])
+    const notes = useNotes(BACKEND_URL)
   
     const handleClick = () => {
       setCounter(counter + 1)
@@ -16,6 +33,7 @@ const App = () => {
         <button onClick={handleClick}>
           press
         </button>
+        <div>{notes.length} notes on server {BACKEND_URL}</div>
       </div>
     )
   }
