02357d8fb7fb9a6c0268f845a7338d4ee5c165ec
Patricio Sobrado <patricio.sobrado@gmail.com>
2024-01-29 13:37:53 -0300

    Handling mutation errors

--- part8/apollo-client/src/App.js
+++ part8/apollo-client/src/App.js
@@ -1,20 +1,31 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
-
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
+
+import Notify from './components/Notify'
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   if (result.loading)  {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons}/>
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   )
 }
