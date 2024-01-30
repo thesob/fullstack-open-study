ce2c6b5e1fb10f027a8824f5785e7e15715c1a4d
Patricio Sobrado <patricio.sobrado@gmail.com>
2024-01-30 17:21:31 -0300

    part 8.c validation

--- part8/apollo-server/index.js
+++ part8/apollo-server/index.js
@@ -1,7 +1,24 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const { v1: uuid } = require('uuid')
 const { GraphQLError } = require('graphql')
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const Person = require('./models/person')
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
 
 let persons = [
   {
@@ -27,26 +44,32 @@ let persons = [
 ]
 
 const typeDefs = `
-type Address {
-  street: String!
-  city: String! 
-}
+  type Address {
+    street: String!
+    city: String! 
+  }
 
-type Person {
-  name: String!
-  phone: String
-  address: Address!
-  id: ID!
-}
-
-enum YesNo {
-  YES
-  NO
-}
-
+  enum YesNo {
+    YES
+    NO
+  }
+  
   type Query {
     personCount: Int!
     allPersons(phone: YesNo): [Person!]!
+    findPerson(name: String!): Person
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -57,6 +80,7 @@ enum YesNo {
       street: String!
       city: String!
     ): Person
+
     editNumber(
       name: String!
       phone: String!
@@ -66,51 +90,58 @@ enum YesNo {
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
-    allPersons: (root, args) => {
+    personCount: async () => Person.collection.countDocuments(),
+    allPersons: async (root, args) => {
       if (!args.phone) {
-        return persons
+        return Person.find({})
       }
-      const byPhone = (person) =>
-        args.phone === 'YES' ? person.phone : !person.phone
-      return persons.filter(byPhone)
+  
+      return Person.find({ phone: { $exists: args.phone === 'YES'  }})
     },
-    findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
   },
   Person: {
-    address: (root) => {
-      return { 
-        street: root.street,
-        city: root.city
+    address: ({ street, city }) => {
+      return {
+        street,
+        city,
       }
-    }
+    },
   },
   Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find(p => p.name === args.name)) {
-        throw new GraphQLError('Name must be unique', {
+    addPerson: async (root, args) => {
+      const person = new Person({ ...args })
+      try {
+        await person.save()
+      } catch (error) {
+        throw new GraphQLError('Saving user failed', {
           extensions: {
             code: 'BAD_USER_INPUT',
-            invalidArgs: args.name
+            invalidArgs: args.name,
+            error
           }
         })
       }
 
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
       return person
     },
-    editNumber: (root, args) => {
-      const person = persons.find(p => p.name === args.name)
-      if (!person) {
-        return null
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone
+      try {
+        await person.save()
+      } catch (error) {
+        throw new GraphQLError('Editing number failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
       }
-  
-      const updatedPerson = { ...person, phone: args.phone }
-      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
-      return updatedPerson
-    }  
+
+      return person
+    },
   }
 }
 
@@ -118,6 +149,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
+
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
